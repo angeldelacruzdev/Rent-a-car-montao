8ed2d7a47de385eec4c2e132c8082f8ca89eb5d4
AngelSBM <angelsebastianbellomateo@gmail.com>
2021-05-02 23:19:18 -0400

    DisPlantilla para diseñar pantallas finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './pages/page1/page1.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: Page1Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
